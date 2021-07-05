fb283abf04e2f63eb2f2527d488f9154fbd63412
Senya <senya@riseup.net>
2021-07-06 00:25:17 +0300

    Basic image opener using konva

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import {Image, Layer, Stage, Text} from "react-konva";
+import {useState} from "react";
+import useImage from "use-image";
 
 function App() {
+  const [currentFile, setCurrentFile] = useState(null)
+  const [image] = useImage(currentFile);
+  const width = (image && image.width) || 100
+  const height = (image && image.height) || 100
+  const onFileSelect = (event) => {
+    const fr = new FileReader();
+    fr.onloadend = () => {
+      setCurrentFile(fr.result)
+    }
+    fr.readAsDataURL(event.target.files[0]);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Stage width={width} height={height}>
+        <Layer>
+            <Image image={image} />
+        </Layer>
+      </Stage>
+        <input type="file" onChange={onFileSelect} />
     </div>
   );
 }
